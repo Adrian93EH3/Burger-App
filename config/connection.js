ccc6ffe6178956e29b40e8da90e7c13b0529005d
Adrian Diaz <adrian93eh3@gmail.com>
2020-09-10 22:31:38 -0400

    using info from PDF to follow the steps to deploy app with MySQL using Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,28 @@
-// Set up MySQL connection.
+// Set up MySQL connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Aqua5961!",
-  database: "burger_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Aqua5961!",
+//   database: "burger_db"
+// });
 
-// Make connection.
+// 
+if (process.env.JAWSB_URL) {
+    connection = mysql.createConnection(process.env.JAWSB_URL);
+} else {
+    connection = mysql.createConnection( {
+        host: "localhost",
+        user: "root",
+        password: "hacktheplanet",
+        database: "todoagain_db"
+    });
+};
+
+// Make connection
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -18,5 +31,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+// Export connection for our ORM to use
 module.exports = connection;
